704c423ea2a6aac2a320cda512f6eabd18680d88
Anton <beetlea@ya.ru>
2022-11-29 13:40:18 +0300

    add draft

--- bitcoin_wallet.py
+++ bitcoin_wallet.py
@@ -1,10 +1,18 @@
 #https://tirinox.ru/send-bitcoin-over-python/?ysclid=lb1xuaxi9z988165321
 from bit import PrivateKeyTestnet as Key
+
 k = Key()
 wallet_my = k.to_wif()
 print('Private key:', k.to_wif())
 print('Public address:', k.address)
 
-
-k = Key(wallet_my)
+#k = Key("cQTtf1ZGfJd8k4Jcq4F5UYREYCHdQtwSy6DMyiy4T1iWYGPZ4bGE")##public mwe1UYZXZ48nLnSr1H4TkVjasKVopHgRjc
 print(k.get_balance())  # 1391025
+
+#source_k = Key('cQTtf1ZGfJd8k4Jcq4F5UYREYCHdQtwSy6DMyiy4T1iWYGPZ4bGE')
+#dest_k = Key('bc1q9jutuqhax5dh2c3k9r60hwdrvstkx893t5zky0')
+print(f'Send from {k.address} to {"bc1q9jutuqhax5dh2c3k9r60hwdrvstkx893t5zky0"}')
+r = k.send([
+    ("bc1q9jutuqhax5dh2c3k9r60hwdrvstkx893t5zky0", 0.01972653, 'btc')
+])
+print(r)  # ID транзакции
